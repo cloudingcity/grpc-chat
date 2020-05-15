a6ce5d62bbf460a21ee70e91c8ee753ea429bdf4
clouding <ghost0436@gmail.com>
2020-05-16 01:33:44 +0800

    Implement user manager

---
+++ internal/server/manager.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"errors"
+	"sync"
+
+	pb "github.com/cloudingcity/grpc-chat/proto"
+)
+
+type User struct {
+	Token  token
+	Name   string
+	Stream pb.Chat_StreamServer
+	Done   chan struct{}
+}
+
+type UserManager struct {
+	users map[token]*User
+	mux   sync.RWMutex
+}
+
+func NewUserManager() *UserManager {
+	return &UserManager{
+		users: make(map[token]*User),
+	}
+}
+
+func (m *UserManager) Users() map[token]*User {
+	return m.users
+}
+
+func (m *UserManager) Register(tkn token, username string) {
+	m.mux.Lock()
+	m.users[tkn] = &User{
+		Token: tkn,
+		Name:  username,
+		Done:  make(chan struct{}),
+	}
+	m.mux.Unlock()
+}
+
+func (m *UserManager) Get(tkn token) (*User, error) {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	if user, ok := m.users[tkn]; ok {
+		return user, nil
+	}
+	return nil, errors.New("token not valid")
+}
+
+func (m *UserManager) Remove(tkn token) {
+	m.mux.Lock()
+	delete(m.users, tkn)
+	m.mux.Unlock()
+}
---
+++ internal/server/manager_test.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestManager(t *testing.T) {
+	const tkn1, tkn2 = "token 1", "token 2"
+	m := NewUserManager()
+
+	m.Register(tkn1, "Foo")
+	m.Register(tkn2, "Bar")
+	require.Equal(t, 2, len(m.Users()))
+
+	_, err := m.Get("not exists token")
+	require.Error(t, err)
+
+	user, err := m.Get(tkn1)
+	require.NoError(t, err)
+	require.Equal(t, "Foo", user.Name)
+
+	m.Remove(tkn1)
+	require.Equal(t, 1, len(m.users))
+}
